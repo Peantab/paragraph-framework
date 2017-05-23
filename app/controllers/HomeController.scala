70fc4f661fe068fb11a1809b33cd4192fafdee88
Paweł Taborowski <paweltab@wp.pl>
2017-05-23 15:32:35 +0200

    Initial commit

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,44 @@
+package controllers
+
+import javax.inject._
+
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.data.format.Formats._
+import play.api.i18n.I18nSupport
+import play.api.i18n.MessagesApi
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class HomeController @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
+
+  /**
+   * Create an Action to render an HTML page with a welcome message.
+   * The configuration in the `routes` file means that this method
+   * will be called when the application receives a `GET` request with
+   * a path of `/`.
+   */
+  def index = Action {
+    Ok(views.html.index(xmlForm))
+  }
+
+  def button = Action { implicit request =>
+    Ok(views.html.index(xmlForm))
+  }
+
+  val xmlForm = Form(
+    mapping(
+      "xml" -> nonEmptyText
+    )(UserXML.apply)(UserXML.unapply)
+  )
+}
+
+case class UserXML(xml: String)
+//object HomeController{
+//
+//}
